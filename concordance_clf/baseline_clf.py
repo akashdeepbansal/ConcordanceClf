34dd6a7fa02b16904debdcf94abecbde61c22164
pawan <pkumar2818@gmail.com>
2021-01-06 21:40:30 +0530

    Counter({0: 4834, 1: 1135}) Naive Bayes:--------------- Total Accuracy: 0.8417085427135679 f1_score: 0.6651576459276471 confusion matrix labels[1,0]: [[ 69 158]  [ 31 936]] SVM:--------------- Total Accuracy: 0.8115577889447236 f1_score: 0.4566261672980578 confusion matrix labels[1,0]: [[  2 225]  [  0 967]]

--- concordance_clf/baseline_clf.py
+++ concordance_clf/baseline_clf.py
@@ -36,39 +36,11 @@ except FileNotFoundError as file_error:
                 annotation = 1 if sum([int(annot) for annot in annotation_list]) >= 2 else 0
 
             dataset.loc[idx] = [json_item["concordanceText"], annotation]
-
+    print(Counter(dataset["annot"].tolist()))
     with open("../logs/panda_dataset.pkl", 'wb') as f_write:
         pickle.dump(dataset, f_write)
         # print(dataset["annot"])
 # creating the one-hot encoding for the words based
-try:
-    with open("../logs/vectorizer.pkl", 'rb') as f_read:
-        vectorizer = pickle.load(f_read)
-    with open("../logs/dataset_X.pkl", 'rb') as f_read:
-        dataset_X = pickle.load(f_read)
-    with open("../logs/dataset_y.pkl", 'rb') as f_read:
-        dataset_y = pickle.load(f_read)
-
-except FileNotFoundError as file_error:
-    from sklearn.feature_extraction.text import CountVectorizer
-
-    vectorizer = CountVectorizer(lowercase=False)
-    dataset_X = vectorizer.fit_transform(dataset["concor"].tolist())
-    dataset_y = dataset['annot'].tolist()
-    # print("Vocab: ")
-    # print(vectorizer.vocabulary_)
-    # print(dataset_X[0])
-    # print(vectorizer.inverse_transform(dataset_X[0]))
-    with open("../logs/vectorizer.pkl", 'wb') as f_write:
-        pickle.dump(vectorizer, f_write)
-    with open("../logs/dataset_X.pkl", 'wb') as f_write:
-        pickle.dump(dataset_X, f_write)
-    with open("../logs/dataset_y.pkl", 'wb') as f_write:
-        pickle.dump(dataset_y, f_write)
-
-# print(dataset_X[1].toarray())
-# print(dataset_y[1])
-# print(vectorizer.inverse_transform(dataset_X[1]))
 try:
     with open("../logs/train_X.pkl", 'rb') as f_read:
         train_X = pickle.load(f_read)
@@ -78,11 +50,23 @@ try:
         test_X = pickle.load(f_read)
     with open("../logs/test_y.pkl", 'rb') as f_read:
         test_y = pickle.load(f_read)
-except FileNotFoundError as file_error:
-    from sklearn.model_selection import train_test_split
 
+except FileNotFoundError as file_error:
+    # from sklearn.feature_extraction.text import CountVectorizer
+    # vectorizer = CountVectorizer(lowercase=False)
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    vectorizer = TfidfVectorizer(stop_words={'english'})
+    from sklearn.model_selection import train_test_split
+    dataset_X = dataset["concor"].tolist()
+    dataset_y = dataset['annot'].tolist()
     train_X, test_X, train_y, test_y = train_test_split(dataset_X, dataset_y, test_size=0.2, random_state=1,
                                                         stratify=dataset_y)
+    train_X = vectorizer.fit_transform(train_X)
+    test_X = vectorizer.transform(test_X)
+    # print("Vocab: ")
+    # print(vectorizer.vocabulary_)
+    # print(dataset_X[0])
+    # print(vectorizer.inverse_transform(dataset_X[0]))
     with open("../logs/train_X.pkl", 'wb') as f_write:
         pickle.dump(train_X, f_write)
     with open("../logs/train_y.pkl", 'wb') as f_write:
@@ -91,6 +75,11 @@ except FileNotFoundError as file_error:
         pickle.dump(test_X, f_write)
     with open("../logs/test_y.pkl", 'wb') as f_write:
         pickle.dump(test_y, f_write)
+
+# print(dataset_X[1].toarray())
+# print(dataset_y[1])
+# print(vectorizer.inverse_transform(dataset_X[1]))
+
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
@@ -101,6 +90,20 @@ accuracy = metrics.accuracy_score(test_y, prediction)
 f1_score = metrics.f1_score(test_y, prediction, average='macro')
 conf_mat = metrics.confusion_matrix(test_y, prediction, labels=[1, 0] )
 
+print("Naive Bayes:---------------")
+print(f"Total Accuracy: {accuracy}")
+print(f"f1_score: {f1_score}")
+print(f'confusion matrix labels[1,0]:\n{conf_mat}')
+
+from sklearn.linear_model import SGDClassifier
+clf_svm = SGDClassifier(loss='hinge', alpha=1e-3, random_state=1)
+clf_svm.fit(train_X, train_y)
+prediction = clf_svm.predict(test_X)
+accuracy = metrics.accuracy_score(test_y, prediction)
+f1_score = metrics.f1_score(test_y, prediction, average='macro')
+conf_mat = metrics.confusion_matrix(test_y, prediction, labels=[1, 0] )
+
+print("SVM:---------------")
 print(f"Total Accuracy: {accuracy}")
 print(f"f1_score: {f1_score}")
 print(f'confusion matrix labels[1,0]:\n{conf_mat}')
